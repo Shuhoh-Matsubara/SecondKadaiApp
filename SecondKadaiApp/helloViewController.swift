7d504dac1ec3fafd6262932e3b1dfe512f6d784c
shuhoh.matsubara <MatsubaraShuhoh@Pams-BagdadCafe.local>
2016-09-27 16:53:45 +0900

    パーツの配置と遷移まで作成。

---
+++ SecondKadaiApp/helloViewController.swift
@@ -0,0 +1,37 @@
+//
+//  helloViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 松原秀峰 on 2016/09/27.
+//  Copyright © 2016年 松原秀峰. All rights reserved.
+//
+
+import UIKit
+
+class helloViewController: UIViewController {
+
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
